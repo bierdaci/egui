48325ba122f4b06ab31a241a7e77bd79d726ab1f
bierdaci <bierdaci@qq.com>
2014-12-27 14:13:10 +0800

    add ewinconfig.h

---
+++ ewinconfig.h
@@ -0,0 +1,11 @@
+/* Define if support FREETYPE font */
+/* #undef _GAL_SUPPORT_FREETYPE */
+
+/* Define if support JPEG file format */
+#define _GAL_SUPPORT_JPEG 1
+
+/* Define if support OpenGL file format */
+#define _GAL_SUPPORT_OPENGL 1
+
+/* Define if support PNG file format */
+#define _GAL_SUPPORT_PNG 1
