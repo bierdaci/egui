4f80bb3939a960a59d79bf1115f74ca49d076fed
bierdaci <bierdaci@qq.com>
2014-12-27 15:21:52 +0800

    modify elist.c elist.h

--- elib/elist.c
+++ elib/elist.c
@@ -18,7 +18,7 @@ elist_t *e_list_new(int size)
 	return lt;
 }
 
-void e_list_insert_head(elist_t *lt, enode_t *new)
+void e_list_insert_head(elist_t *lt, elistnode_t *new)
 {
 	if (!lt->head) {
 		new->next = 0;
@@ -36,7 +36,7 @@ void e_list_insert_head(elist_t *lt, enode_t *new)
 	}
 }
 
-void e_list_insert_tail(elist_t *lt, enode_t *new)
+void e_list_insert_tail(elist_t *lt, elistnode_t *new)
 {
 	if (!lt->tail) {
 		new->next = 0;
@@ -54,7 +54,7 @@ void e_list_insert_tail(elist_t *lt, enode_t *new)
 	}
 }
 
-void e_list_insert_after(elist_t *lt, enode_t *node, enode_t *new)
+void e_list_insert_after(elist_t *lt, elistnode_t *node, elistnode_t *new)
 {
 	if (!lt->head) {
 		new->next = 0;
@@ -80,7 +80,7 @@ void e_list_insert_after(elist_t *lt, enode_t *node, enode_t *new)
 		e_list_insert_head(lt, new);
 }
 
-void e_list_insert_before(elist_t *lt, enode_t *node, enode_t *new)
+void e_list_insert_before(elist_t *lt, elistnode_t *node, elistnode_t *new)
 {
 	if (!lt->head) {
 		new->next = 0;
@@ -106,7 +106,7 @@ void e_list_insert_before(elist_t *lt, enode_t *node, enode_t *new)
 		e_list_insert_tail(lt, new);
 }
 
-void e_list_remove(elist_t *lt, enode_t *node)
+void e_list_remove(elist_t *lt, elistnode_t *node)
 {
 	if (node == lt->tail && node == lt->head) {
 		lt->head = 0;
@@ -127,14 +127,14 @@ void e_list_remove(elist_t *lt, enode_t *node)
 	lt->count--;
 }
 
-enode_t *e_list_alloc_node(elist_t *lt, void *data)
+elistnode_t *e_list_alloc_node(elist_t *lt, void *data)
 {
-	enode_t *node = malloc(sizeof(enode_t) + lt->size);
+	elistnode_t *node = malloc(sizeof(elistnode_t) + lt->size);
 	memcpy(node + 1, data, lt->size);
 	return node;
 }
 
-void e_list_free_node(elist_t *lt, enode_t *node)
+void e_list_free_node(elist_t *lt, elistnode_t *node)
 {
 	e_list_remove(lt, node);
 	free(node);
@@ -142,9 +142,9 @@ void e_list_free_node(elist_t *lt, enode_t *node)
 
 void e_list_empty(elist_t *lt)
 {
-	enode_t *node = lt->head;
+	elistnode_t *node = lt->head;
 	while (node) {
-		enode_t *t = node;
+		elistnode_t *t = node;
 		node = node->next;
 		free(t);
 	}
@@ -155,20 +155,20 @@ void e_list_empty(elist_t *lt)
 
 void e_list_add_by_copy(elist_t *lt, void *data)
 {
-	enode_t *node = e_list_alloc_node(lt, data);
+	elistnode_t *node = e_list_alloc_node(lt, data);
 	e_list_insert_tail(lt, node);
 }
 
 void e_list_add_by_copy_size(elist_t *lt, void *data, int size)
 {
-	enode_t *node = e_list_alloc_node(lt, data);
+	elistnode_t *node = e_list_alloc_node(lt, data);
 	e_list_insert_tail(lt, node);
 }
 
-void e_list_add_by_sort_copy_size(elist_t *lt, void *data, int size, int (*cb)(enode_t *, enode_t *))
+void e_list_add_by_sort_copy_size(elist_t *lt, void *data, int size, int (*cb)(elistnode_t *, elistnode_t *))
 {
-	enode_t *node = lt->head;
-	enode_t *new  = e_list_alloc_node(lt, data);
+	elistnode_t *node = lt->head;
+	elistnode_t *new  = e_list_alloc_node(lt, data);
 
 	while (node) {
 		if (cb(node, new))
@@ -180,13 +180,13 @@ void e_list_add_by_sort_copy_size(elist_t *lt, void *data, int size, int (*cb)(e
 
 void e_list_add_data(elist_t *lt, void *data)
 {
-	enode_t *node = e_list_alloc_node(lt, data);
+	elistnode_t *node = e_list_alloc_node(lt, data);
 	e_list_insert_tail(lt, node);
 }
 
 void e_list_add_data_to_head(elist_t *lt, void *data)
 {
-	enode_t *node = e_list_alloc_node(lt, data);
+	elistnode_t *node = e_list_alloc_node(lt, data);
 	e_list_insert_head(lt, node);
 }
 
@@ -208,7 +208,7 @@ void e_list_out_data_from_tail(elist_t *lt, void *buf)
 	}
 }
 
-void e_list_copy_data(elist_t *lt, enode_t *node, void *buf)
+void e_list_copy_data(elist_t *lt, elistnode_t *node, void *buf)
 {
 	memcpy(buf, e_list_get_data(node), lt->size);
 }
@@ -216,7 +216,7 @@ void e_list_copy_data(elist_t *lt, enode_t *node, void *buf)
 void *e_list_next_data(elist_t *lt, void *data)
 {
 	if (data) {
-		enode_t *node = (enode_t *)data - 1;
+		elistnode_t *node = (elistnode_t *)data - 1;
 		return e_list_get_data(node->next);
 	}
 	else if (lt->head)
@@ -228,7 +228,7 @@ void *e_list_next_data(elist_t *lt, void *data)
 void *e_list_prev_data(elist_t *lt, void *data)
 {
 	if (data) {
-		enode_t *node = (enode_t *)data - 1;
+		elistnode_t *node = (elistnode_t *)data - 1;
 		return e_list_get_data(node->prev);
 	}
 	else if (lt->tail)
@@ -239,13 +239,13 @@ void *e_list_prev_data(elist_t *lt, void *data)
 
 void e_list_free_data(elist_t *lt, void *data)
 {
-	e_list_free_node(lt, (enode_t *)data - 1);
+	e_list_free_node(lt, (elistnode_t *)data - 1);
 }
 
 void e_list_add_data_by_sort(elist_t *lt, void *data)
 {
-	enode_t *node = lt->head;
-	enode_t *new  = e_list_alloc_node(lt, data);
+	elistnode_t *node = lt->head;
+	elistnode_t *new  = e_list_alloc_node(lt, data);
 
 	if (lt->sort) {
 		while (node) {
@@ -259,19 +259,19 @@ void e_list_add_data_by_sort(elist_t *lt, void *data)
 		e_list_insert_tail(lt, node);
 }
 
-void e_list_insert_data_before(elist_t *lt, enode_t *node, void *data)
+void e_list_insert_data_before(elist_t *lt, elistnode_t *node, void *data)
 {
-	enode_t *new = e_list_alloc_node(lt, data);
+	elistnode_t *new = e_list_alloc_node(lt, data);
 	e_list_insert_before(lt, node, new);
 }
 
-void e_list_insert_data_after(elist_t *lt, enode_t *node, void *data)
+void e_list_insert_data_after(elist_t *lt, elistnode_t *node, void *data)
 {
-	enode_t *new = e_list_alloc_node(lt, data);
+	elistnode_t *new = e_list_alloc_node(lt, data);
 	e_list_insert_after(lt, node, new);
 }
 
-void *e_list_get_data(enode_t *node)
+void *e_list_get_data(elistnode_t *node)
 {
 	if (node)
 		return (void *)(node + 1);
@@ -281,7 +281,7 @@ void *e_list_get_data(enode_t *node)
 void *e_list_get_index_data(elist_t *lt, int index)
 {
 	int i;
-	enode_t *node;
+	elistnode_t *node;
 
 	if (index == 0)
 		return e_list_get_data(lt->head);
@@ -305,7 +305,7 @@ void *e_list_get_index_data(elist_t *lt, int index)
 	return NULL;
 }
 
-void e_list_set_func(elist_t *lt, int (*sort)(enode_t *, enode_t *, void *data), void *data)
+void e_list_set_func(elist_t *lt, int (*sort)(elistnode_t *, elistnode_t *, void *data), void *data)
 {
 	lt->sort = sort;
 	lt->data = data;
@@ -313,8 +313,8 @@ void e_list_set_func(elist_t *lt, int (*sort)(enode_t *, enode_t *, void *data),
 
 void e_list_reverse(elist_t *lt)
 {
-	enode_t *last = NULL;
-	enode_t *node = lt->head;
+	elistnode_t *last = NULL;
+	elistnode_t *node = lt->head;
 
     while (node) {
         last = node;
@@ -330,7 +330,7 @@ void e_list_reverse(elist_t *lt)
 
 elist_t *e_list_copy(elist_t *src, elist_t *dst)
 {
-	enode_t *node = src->head;
+	elistnode_t *node = src->head;
 
 	e_list_empty(dst);
 	dst->size  = src->size;
@@ -346,7 +346,7 @@ elist_t *e_list_copy(elist_t *src, elist_t *dst)
 
 elist_t *e_list_reverse_copy(elist_t *src, elist_t *dst)
 {
-	enode_t *node = src->tail;
+	elistnode_t *node = src->tail;
 
 	e_list_empty(dst);
 	dst->size  = src->size;
